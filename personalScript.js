4224139b4e106f113e911b1217275c9015e83cb5
WolvesareElite123 <WolvesareElite123@users.noreply.github.com>
2022-09-18 18:52:01 -0500

    Version 1 Complete

---
+++ personalScript.js
@@ -0,0 +1,45 @@
+﻿function changeButtons(num)
+{
+    for(let x = 1; x<=4; x++)
+    {
+        let current = "btn"+x;
+        if(document.getElementById(current).classList.contains('btn-primary'))
+        {
+            document.getElementById(current).classList.remove('btn-primary');
+        }
+        if(current !== num)
+        {
+            if(!document.getElementById(current).classList.contains('btn-outline-dark'))
+            {
+                document.getElementById(current).classList.add('btn-outline-dark');
+            }
+        }
+    }
+    let temp = "btn"+num;
+    document.getElementById(temp).classList.remove('btn-outline-dark');
+    document.getElementById(temp).classList.add('btn-primary');
+}
+
+function changeDisplay(num)
+{
+    for(let x = 1; x<=4; x++)
+    {
+        let current = "disp"+x;
+        let display = document.getElementById(current);
+        if(x===num)
+        {
+            display.style.display = "block";
+        }
+        else
+        {
+            display.style.display = "none";
+        }
+    }
+}
+
+//Something was clicked
+function btnClick(num)
+{
+    changeButtons(num);
+    changeDisplay(num);
+}
